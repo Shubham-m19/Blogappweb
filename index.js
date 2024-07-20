06e06150898632fb5e2bd3dad77d2cece24f4db0
Shubham <mhatukadeshubham9462@gmail.com>
2024-07-20 22:11:33 +0530

    ejs and bootstrap

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT;
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules/bootstrap"));
+
+app.get("/", (req, res) => {
+  
+  res.render(__dirname + "/views/index");
+});
+
+app.get("/about", (req, res) => {
+  
+  res.render(__dirname + "/views/About");
+});
+
+app.get("/contact", (req, res) => {
+  
+  res.render(__dirname + "/views/Contact");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
